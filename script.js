ede52d3829105e76b3273b8b38b864d038797bbf
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-06-20 20:36:46 +0100

    my todo list

---
+++ script.js
@@ -0,0 +1,93 @@
+// declaring our variables
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const taskContainer = document.querySelector("#tasks");
+const error = document.querySelector("#error");
+const countValue = document.querySelector(".count-value");
+let taskCount = 0;
+
+const displayCount = (taskCount) => {
+    countValue.innerText = taskCount;
+}
+
+const addTask = () => {
+    const taskName = newTaskInput.value.trim();
+
+    // error message if the field is empty.
+    error.style.display = "none";
+    if(!taskName){
+        setTimeout( () => {
+            error.style.display = "block";
+        }, 200);
+        return;
+    }
+
+    const task = `<div class = "task">
+        <input type= "checkbox" class= "task-check">
+        <span class= "taskname">${taskName}</span>
+
+        <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+        <button class="delete"><i class="fa-solid fa-trash"></i></button>
+    </div>`;
+
+    taskContainer.insertAdjacentHTML("beforeend", task);
+    taskCount++;
+    displayCount(taskCount);
+    
+
+
+    // delete button scripting.
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach( deleteBtn => {
+        deleteBtn.onclick = () => {
+            deleteBtn.parentNode.remove();
+            if(taskCount > 0){
+                taskCount--;
+            }else{
+                taskCount = 0;
+            }
+            displayCount(taskCount);
+        }
+    });
+
+    // edit button scripting.
+    const editButtons = document.querySelectorAll(".edit");
+    editButtons.forEach( editBtn => {
+        editBtn.onclick = (e) => {
+            let targetElement = e.target;
+            if(!(e.target.className == "edit")){
+                targetElement = e.target.parentElement;
+            }
+
+            newTaskInput.value = targetElement.
+            previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            taskCount--;
+            displayCount(taskCount);
+        }
+    });
+
+    // task check scripting
+    const taskCheck = document.querySelectorAll(".task-check");
+    taskCheck.forEach(box => {
+        box.onchange = () => {
+            box.nextElementSibling.classList.toggle("completed");
+            if(box.checked){
+                taskCount--;
+            }else{
+                taskCount++;
+            }
+            displayCount(taskCount);
+        }
+    });
+    // displayCount(taskCount);
+    newTaskInput.value = "";
+}
+
+addBtn.addEventListener("click", addTask);
+
+window.onload = () => {
+    taskCount = 0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+}
